860c7393dbda81ca8f01fe1cd624ddc0edf5aa80
RishabhBhati23 <bhatirishabh.2003@gmail.com>
2023-10-17 22:03:10 +0530

    added problem 28 successfully GCD

---
+++ problem29/GCD/problem29.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class problem29{
+    public static void main(String[] args){
+        int num1 = 36;  int num2 = 60 ; int gcd=0;
+
+        for(int i=1 ; i<=num1 || i<= num2 ;i++){
+            if(num1%i==0 && num2%i==0){
+                gcd = i;
+            }
+        }
+
+        //greatest common divisors
+
+        System.out.println("GCD is :-" + gcd);
+    }
+}
